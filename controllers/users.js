f99bb78eb41958b266c889dff3c10133be882fac
Taiwo Enoch <enochtaiwotimothy@gmail.com>
2021-06-29 09:25:45 +0100

    changing some stuffs

--- controllers/users.js
+++ controllers/users.js
@@ -140,7 +140,7 @@ let users = {
             User.find({}).sort('count').exec(function(err, docs) { 
                 if(!err && docs){
                     let sort = docs.sort(function(a, b){return b.count - a.count})
-                 res.render('login',{message:"successful",users:sort,user_id:req.user_data});
+                 res.render('dashboard',{message:"successful",users:sort,user_id:req.user_data});
                 }
                 else{ 
                  res.render('dashboard',{message:"error occure"});
