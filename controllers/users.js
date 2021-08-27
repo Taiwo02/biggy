b2069c464e7d51687b09fa665cbd4029d752a18e
Taiwo <45623496+Taiwo02@users.noreply.github.com>
2021-08-27 17:05:21 +0100

    change re to res

--- controllers/users.js
+++ controllers/users.js
@@ -52,7 +52,7 @@ let users = {
                         }
                     }
                     else{
-                        re.redirect("/")
+                        res.redirect("/")
                     }
                 })
                
